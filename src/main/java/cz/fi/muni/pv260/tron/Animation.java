ea0d875d0ab62c17ea946bb9d51253319c1efc69
ankur <ankur.lohchab@gmail.com>
2020-03-23 16:23:28 -0800

    comment out unused methods

--- src/main/java/cz/fi/muni/pv260/tron/Animation.java
+++ src/main/java/cz/fi/muni/pv260/tron/Animation.java
@@ -1,5 +1,6 @@
 package main.java.cz.fi.muni.pv260.tron;
 
+
 import java.awt.Image;
 import java.util.ArrayList;
 public class Animation {
@@ -9,11 +10,13 @@ public class Animation {
 	private long movieTime;
 	private long totalTime;
 
-	public Animation(){
-		scenes = new ArrayList();
-		totalTime = 0;
-		start();
-	}
+// --Commented out by Inspection START (23-03-2020 13:12):
+//	public Animation(){
+//		scenes = new ArrayList();
+//		totalTime = 0;
+//		start();
+//	}
+// --Commented out by Inspection STOP (23-03-2020 13:12)
 
 // --Commented out by Inspection START (23-03-2020 12:58):
 //	public synchronized void addScene(Image i, long t){
@@ -27,27 +30,31 @@ public class Animation {
 		sceneIndex = 0;
 	}
 
-	public synchronized void update(long timePassed){
-		if(scenes.size()>1){
-			movieTime += timePassed;
-			if(movieTime>=totalTime){
-				movieTime = 0;
-				sceneIndex = 0;
-			}
-			while(movieTime > getScene(sceneIndex).endTime){
-				sceneIndex++;
-			}
-		}
-	}
+// --Commented out by Inspection START (23-03-2020 13:12):
+//	public synchronized void update(long timePassed){
+//		if(scenes.size()>1){
+//			movieTime += timePassed;
+//			if(movieTime>=totalTime){
+//				movieTime = 0;
+//				sceneIndex = 0;
+//			}
+//			while(movieTime > getScene(sceneIndex).endTime){
+//				sceneIndex++;
+//			}
+//		}
+//	}
+// --Commented out by Inspection STOP (23-03-2020 13:12)
 
-	public synchronized Image getImage(){
-		if(scenes.size()==0)
-		{
-			return null;
-		}else{
-			return getScene(sceneIndex).pic;
-		}
-	}
+// --Commented out by Inspection START (23-03-2020 13:12):
+//	public synchronized Image getImage(){
+//		if(scenes.size()==0)
+//		{
+//			return null;
+//		}else{
+//			return getScene(sceneIndex).pic;
+//		}
+//	}
+// --Commented out by Inspection STOP (23-03-2020 13:12)
 
 	private oneScene getScene(int x){
 		return (oneScene)scenes.get(x);
@@ -57,9 +64,11 @@ public class Animation {
 		Image pic;
 		long endTime;
 
-		public oneScene(Image pic,long endTime){
-			this.pic = pic;
-			this.endTime = endTime;
-		}
+// --Commented out by Inspection START (23-03-2020 13:12):
+//		public oneScene(Image pic,long endTime){
+//			this.pic = pic;
+//			this.endTime = endTime;
+//		}
+// --Commented out by Inspection STOP (23-03-2020 13:12)
 	}
 }
