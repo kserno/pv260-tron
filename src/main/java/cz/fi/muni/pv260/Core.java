56002929d2be4ee45373421aff2fd75e54901c66
ankur <ankur.lohchab@gmail.com>
2020-04-01 13:29:01 -0800

    refactoring - extract methods

--- src/main/java/cz/fi/muni/pv260/Core.java
+++ src/main/java/cz/fi/muni/pv260/Core.java
@@ -9,9 +9,8 @@ public abstract class Core implements KeyListener, MouseListener,
 
 	private static final DisplayMode modes[] = 
 		{
-		//new DisplayMode(1920,1080,32,0),
+
 		new DisplayMode(1680,1050,32,0),
-		//new DisplayMode(1280,1024,32,0),
 		new DisplayMode(800,600,32,0),
 		new DisplayMode(800,600,24,0),
 		new DisplayMode(800,600,16,0),
@@ -36,22 +35,32 @@ public abstract class Core implements KeyListener, MouseListener,
 	}
 	
 	public void init(){
-		sm = new ScreenManager();
-		DisplayMode dm = sm.findFirstCompatibaleMode(modes);
-		sm.setFullScreen(dm);
-		Window w = sm.getFullScreenWindow();
+		Window w = getWindowScreen();
+		setWindowScreen(w);
+		addListeners(w);
+		running = true;
+	}
+
+	private void addListeners(Window w) {
+		w.addKeyListener(this);
+		w.addMouseListener(this);
+		w.addMouseMotionListener(this);
+	}
+
+	private void setWindowScreen(Window w) {
 		w.setFont(new Font("Arial",Font.PLAIN,20));
 		w.setBackground(Color.WHITE);
 		w.setForeground(Color.RED);
 		w.setCursor(w.getToolkit().createCustomCursor(new BufferedImage(3, 3, BufferedImage.TYPE_INT_ARGB), new Point(0, 0),"null"));
-
-		w.addKeyListener(this);
-		w.addMouseListener(this);
-		w.addMouseMotionListener(this);
-
-		running = true;
 	}
-	
+
+	private Window getWindowScreen() {
+		sm = new ScreenManager();
+		DisplayMode dm = sm.findFirstCompatibaleMode(modes);
+		sm.setFullScreen(dm);
+		return sm.getFullScreenWindow();
+	}
+
 	public void gameLoop(){
 		long startTime = System.currentTimeMillis();
 		long cumTime = startTime;
