bf5e0014b6f37f8c29fc17b7995851916fbdc2eb
ankur <ankur.lohchab@gmail.com>
2020-04-01 13:45:03 -0800

    refactoring - extract methods

--- src/main/java/cz/fi/muni/pv260/ScreenManager.java
+++ src/main/java/cz/fi/muni/pv260/ScreenManager.java
@@ -2,7 +2,6 @@ package cz.fi.muni.pv260;
 
 import java.awt.*;
 import java.awt.image.BufferStrategy;
-import java.awt.image.BufferedImage;
 
 import javax.swing.JFrame;
 
@@ -33,18 +32,33 @@ public class ScreenManager {
 	}
 
 	public boolean displayModesMatch(DisplayMode m1, DisplayMode m2){
-		if(m1.getWidth() != m2.getWidth() || m1.getHeight() != m2.getHeight()){
-			return false;
-		}
-		if(m1.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m2.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m1.getBitDepth() != m2.getBitDepth()){
-			return false;
-		}
-		if(m1.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN && m2.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN && m1.getRefreshRate() != m2.getRefreshRate()){
-			return false;
-		}
+		if (checkWidth(m1, m2)) return false;
+		if (checkDepth(m1, m2)) return false;
+		if (checkRefreshRate(m1, m2)) return false;
 		return true;
 	}
-	
+
+	private boolean checkRefreshRate(DisplayMode m1, DisplayMode m2) {
+		if(m1.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN && m2.getRefreshRate() != DisplayMode.REFRESH_RATE_UNKNOWN && m1.getRefreshRate() != m2.getRefreshRate()){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkDepth(DisplayMode m1, DisplayMode m2) {
+		if(m1.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m2.getBitDepth() != DisplayMode.BIT_DEPTH_MULTI && m1.getBitDepth() != m2.getBitDepth()){
+			return true;
+		}
+		return false;
+	}
+
+	private boolean checkWidth(DisplayMode m1, DisplayMode m2) {
+		if(m1.getWidth() != m2.getWidth() || m1.getHeight() != m2.getHeight()){
+			return true;
+		}
+		return false;
+	}
+
 	public void setFullScreen(DisplayMode dm){
 		JFrame f = new JFrame();
 		f.setUndecorated(true);
